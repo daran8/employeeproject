89549a9f564ce40dd6b3e938eb9a11321e5a1c85
daran8 <112875562+daran8@users.noreply.github.com>
2023-07-16 23:13:24 +0530

    Update EmployeeConstants.java

--- com/i2t/constants/EmployeeConstants.java
+++ com/i2t/constants/EmployeeConstants.java
@@ -29,4 +29,5 @@ public class EmployeeConstants {
     public static final String FALSE_MESSAGE = "Somthing Went Wrong!.";
     public static final String ROLE_1 = "Trainer";
     public static final String ROLE_2 = "Trainee";
+    public static final String ROLE_3 = "Viewer";
 }
